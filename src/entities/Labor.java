aa1e46628c4def4a6aa3b012aa0aa7b008681458
khalidwaheli <oukhakhalid@gmail.com>
2024-09-15 17:28:51 +0100

    added menu for clientManagement

--- src/entities/Labor.java
+++ src/entities/Labor.java
@@ -5,8 +5,8 @@ public class Labor extends Component{
     private double workingHours;
     private double workerProductivity;
 
-    public Labor(int id, String name, String componentType, double vatRate, double hourlyRate, double workingHours, double workerProductivity) {
-        super(id, name, componentType, vatRate);
+    public Labor(int id, String name, String componentType, double vatRate, double hourlyRate, double workingHours, double workerProductivity, Project project) {
+        super(id, name, componentType, vatRate,project);
         this.hourlyRate = hourlyRate;
         this.workingHours = workingHours;
         this.workerProductivity = workerProductivity;
