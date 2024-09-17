50065df9aa3c85ac9db6dcb055621ca8e691f1d4
khalidwaheli <oukhakhalid@gmail.com>
2024-09-17 15:59:53 +0100

    fixed menu bug for material management

--- src/views/MaterialManagementMenu.java
+++ src/views/MaterialManagementMenu.java
@@ -10,23 +10,22 @@ import java.util.Scanner;
 
 public class MaterialManagementMenu {
     private Scanner scanner = new Scanner(System.in);
-    private int choice;
     private MaterialHandler materialHandler = new MaterialHandler();
-
 
     public List<Material> showMenu(Project project) {
         List<Material> selectedMaterials = new ArrayList<>();
+        int choice;
 
         do {
-            materialHandler.createMaterial(project);
-            System.out.println("Do you want to add another material? (Y:YES/N:No): ");
-            String addAnotherMaterial = scanner.nextLine();
+            Material material = materialHandler.createMaterial(project);
+            selectedMaterials.add(material);
 
-            if (addAnotherMaterial.equalsIgnoreCase("N")) {
-                choice = 0;
-            }
+            System.out.println("Do you want to add another material? (YES:1 / NO:2): ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
-        } while (choice != 0);
+        } while (choice == 1);
+
         return selectedMaterials;
     }
 }
